9aec75b813ffa08763606a962894684e34d2b09f
Elliott-Roach <elliott.roach@mths.ca>
2024-11-07 10:50:00 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,38 @@
  *
  * Created by: Elliott Roach
  * Created on: nov 2024
- * This program ...
+ * This program transmits to close if 9 cm or closer
 */
 
-basic.showString('Hello, World!')
+//setup
+let distanceToObject: number = 0
+radio.setGroup(218)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//taking distance and sending transmition if 9 cm or less
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+
+    //taking distance forever
+    basic.forever(function() {
+        distanceToObject = sonar.ping(
+            DigitalPin.P15,
+            DigitalPin.P14,
+            PingUnit.Centimeters,
+        )
+    })
+
+    //sending to close trasmition if 9 cm or closer
+    if (distanceToObject < 10) {
+        radio.sendString("To Close")
+        basic.showIcon(IconNames.Happy)
+    }
+})
+
+//displaying transmition if given
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
